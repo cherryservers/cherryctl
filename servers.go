b7dc1f9e43f228c49ded9b1d845097f519ffe39f
Arturas Razinskij <arturas@kazkur.lt>
2019-02-27 08:25:11 +0200

    Refactoring

---
+++ servers.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"cherrygo"
+	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
+)
+
+func addServer(c *cherrygo.Client, projectID string, hostname string, ipaddresses []string, sshKeys []string, image string, planID string, region string) {
+
+	addServerRequest := cherrygo.CreateServer{
+		ProjectID:   projectID,
+		Hostname:    hostname,
+		Image:       image,
+		Region:      region,
+		SSHKeys:     sshKeys,
+		IPAddresses: ipaddresses,
+		PlanID:      planID,
+	}
+
+	server, _, err := c.Server.Create(projectID, &addServerRequest)
+	if err != nil {
+		log.Fatal("Error while creating new server: ", err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
+	fmt.Fprintf(tw, "\n-----\t-------\t----\t---\t----\n")
+	fmt.Fprintf(tw, "Server ID\tHostname\tName\tPTR\tType\n")
+	fmt.Fprintf(tw, "-----\t-------\t----\t---\t----\n")
+	fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\n", server.ID, server.Hostname, server.Name, server.Image, server.State)
+	fmt.Fprintf(tw, "-----\t-------\t----\t---\t----\n")
+	tw.Flush()
+}
+
+func listServers(c *cherrygo.Client, projectID string) {
+
+	// Needs project id to be passed
+	servers, _, err := c.Servers.List(projectID)
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
+	fmt.Fprintf(tw, "\n---------\t----\t--------\t-----\t---\t----\t----------\t--------\t------\t-----\n")
+	fmt.Fprintf(tw, "Server ID\tName\tHostname\tImage\tCPU2\tUnit\tIP address\tSSH keys\tRegion\tState\n")
+	fmt.Fprintf(tw, "---------\t----\t--------\t-----\t---\t----\t----------\t--------\t------\t-----\n")
+
+	for _, srv := range servers {
+
+		for _, i := range srv.IPAddresses {
+			if len(srv.SSHKeys) == 0 {
+				fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\n",
+					srv.ID, srv.Name, srv.Hostname, srv.Image, srv.Plans.Specs.Cpus.Frequency, srv.Plans.Specs.Cpus.Unit, i.Address, nil, srv.Region.Name, srv.State)
+			} else {
+				for _, k := range srv.SSHKeys {
+					fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\n",
+						srv.ID, srv.Name, srv.Hostname, srv.Image, srv.Plans.Specs.Cpus.Frequency, srv.Plans.Specs.Cpus.Unit, i.Address, k.Href, srv.Region.Name, srv.State)
+				}
+			}
+
+		}
+	}
+	fmt.Fprintf(tw, "---------\t----\t--------\t-----\t---\t----\t----------\t--------\t------\t-----\n")
+	tw.Flush()
+
+}
+
+func listServer(c *cherrygo.Client, serverID string) {
+
+	server, _, err := c.Server.List(serverID)
+	if err != nil {
+		log.Fatalf("Error while listing server: %v", err)
+	}
+
+	srvPower, _, err := c.Server.PowerState(serverID)
+	if err != nil {
+		log.Fatalf("Error while getting power sstate: %v", err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
+	fmt.Fprintf(tw, "\n---------\t----\t--------\t-----\t---\t----------\t-----\t-----\n")
+	fmt.Fprintf(tw, "Server ID\tName\tHostname\tImage\tPrice\tIP address\tPower\tState\n")
+	fmt.Fprintf(tw, "---------\t----\t--------\t-----\t---\t----------\t-----\t-----\n")
+	if len(server.IPAddresses) > 0 {
+		for _, i := range server.IPAddresses {
+			fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\n",
+				server.ID, server.Name, server.Hostname, server.Image, server.Pricing.Price, i.Address, srvPower.Power, server.State)
+		}
+	} else {
+		fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\n",
+			server.ID, server.Name, server.Hostname, server.Image, server.Pricing.Price, nil, srvPower.Power, server.State)
+	}
+
+	fmt.Fprintf(tw, "---------\t----\t--------\t-----\t---\t----------\t-----\n")
+	tw.Flush()
+
+}
+
+func deleteServer(c *cherrygo.Client, serverID string) {
+
+	serverDeleteRequest := cherrygo.DeleteServer{ID: serverID}
+
+	c.Server.Delete(&serverDeleteRequest)
+}
+
+func powerOn(c *cherrygo.Client, serverID string) {
+
+	server, _, err := c.Server.PowerOn(serverID)
+	if err != nil {
+		log.Fatal("Error while powering on the server", err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
+	fmt.Fprintf(tw, "\n---------\t----\t--------\t-----\t-----\t-----\n")
+	fmt.Fprintf(tw, "Server ID\tName\tHostname\tImage\tPrice\tState\n")
+	fmt.Fprintf(tw, "---------\t----\t--------\t-----\t-----\t-----\n")
+
+	fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\t%v\n",
+		server.ID, server.Name, server.Hostname, server.Image, server.Pricing.Price, server.State)
+
+	fmt.Fprintf(tw, "---------\t----\t--------\t-----\t-----\t-----\n")
+	tw.Flush()
+
+}
+
+func powerOff(c *cherrygo.Client, serverID string) {
+
+	server, _, err := c.Server.PowerOff(serverID)
+	if err != nil {
+		log.Fatal("Error while powering on the server", err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
+	fmt.Fprintf(tw, "\n---------\t----\t--------\t-----\t-----\t-----\n")
+	fmt.Fprintf(tw, "Server ID\tName\tHostname\tImage\tPrice\tState\n")
+	fmt.Fprintf(tw, "---------\t----\t--------\t-----\t-----\t-----\n")
+
+	fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\t%v\n",
+		server.ID, server.Name, server.Hostname, server.Image, server.Pricing.Price, server.State)
+
+	fmt.Fprintf(tw, "---------\t----\t--------\t-----\t-----\t-----\n")
+	tw.Flush()
+}
+
+func rebootServer(c *cherrygo.Client, serverID string) {
+
+	server, _, err := c.Server.Reboot(serverID)
+	if err != nil {
+		log.Fatal("Error while rebooting the server", err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
+	fmt.Fprintf(tw, "\n---------\t----\t--------\t-----\t-----\t-----\n")
+	fmt.Fprintf(tw, "Server ID\tName\tHostname\tImage\tPrice\tState\n")
+	fmt.Fprintf(tw, "---------\t----\t--------\t-----\t-----\t-----\n")
+
+	fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\t%v\n",
+		server.ID, server.Name, server.Hostname, server.Image, server.Pricing.Price, server.State)
+
+	fmt.Fprintf(tw, "---------\t----\t--------\t-----\t-----\t-----\n")
+	tw.Flush()
+}
---
+++ sshkeys.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"cherrygo"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"text/tabwriter"
+)
+
+func createSSHKey(c *cherrygo.Client, label, key, keyPath string) {
+
+	if keyPath != "" {
+		keyData, err := ioutil.ReadFile(keyPath)
+		if err != nil {
+			log.Fatal("Error while reading key file: ", err)
+		}
+
+		if keyData != nil {
+			key = string(keyData)
+		}
+	}
+
+	sshCreateRequest := cherrygo.CreateSSHKey{
+		Label: label,
+		Key:   key,
+	}
+
+	sshkey, _, err := c.SSHKey.Create(&sshCreateRequest)
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
+	fmt.Fprintf(tw, "\n------\t-----\t-----------\t-------\n")
+	fmt.Fprintf(tw, "KEY ID\tLabel\tFingerprint\tCreated\n")
+	fmt.Fprintf(tw, "------\t-----\t-----------\t-------\n")
+	fmt.Fprintf(tw, "%v\t%v\t%v\t%v\n", sshkey.ID, sshkey.Label, sshkey.Fingerprint, sshkey.Created)
+	fmt.Fprintf(tw, "------\t-----\t-----------\t-------\n")
+	tw.Flush()
+}
+
+func listSSHkeys(c *cherrygo.Client) {
+
+	sshkeys, _, err := c.SSHKeys.List()
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
+	fmt.Fprintf(tw, "\n------\t-----\t-----------\t-------\t-------\n")
+	fmt.Fprintf(tw, "Key ID\tLabel\tFingerprint\tCreated\tUpdated\n")
+	fmt.Fprintf(tw, "------\t-----\t-----------\t-------\t-------\n")
+	for _, s := range sshkeys {
+		fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\n",
+			s.ID, s.Label, s.Fingerprint, s.Created, s.Updated)
+	}
+	fmt.Fprintf(tw, "------\t-----\t-----------\t-------\t-------\n")
+	tw.Flush()
+}
+
+func listSSHkey(c *cherrygo.Client, keyID string) {
+
+	sshkey, _, err := c.SSHKey.List(keyID)
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
+	fmt.Fprintf(tw, "\n------\t-----\t-----------\t-------\t-------\n")
+	fmt.Fprintf(tw, "Key ID\tLabel\tFingerprint\tCreated\tUpdated\n")
+	fmt.Fprintf(tw, "------\t-----\t-----------\t-------\t-------\n")
+
+	fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\n",
+		sshkey.ID, sshkey.Label, sshkey.Fingerprint, sshkey.Created, sshkey.Updated)
+	fmt.Fprintf(tw, "------\t-----\t-----------\t-------\t-------\n")
+	tw.Flush()
+}
+
+func updateSSHKey(c *cherrygo.Client, keyID, keyLabel string) {
+
+	sshUpateRequest := cherrygo.UpdateSSHKey{
+		Label: keyLabel,
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
+	fmt.Fprintf(tw, "\n-------\t-----\t-----------\t-------\t-------\n")
+	fmt.Fprintf(tw, "Key ID\tLabel\tFingerprint\tCreated\tUpdated\n")
+	fmt.Fprintf(tw, "-------\t-----\t-----------\t-------\t-------\n")
+
+	key, _, err := c.SSHKey.Update(keyID, &sshUpateRequest)
+	if err != nil {
+		log.Fatal("Error while updating SSH key", err)
+	}
+
+	fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\n",
+		key.ID, key.Label, key.Fingerprint, key.Created, key.Updated)
+	fmt.Fprintf(tw, "-------\t-----\t-----------\t-------\t-------\n")
+	tw.Flush()
+}
+
+func deleteSSHKey(c *cherrygo.Client, keyID string) {
+
+	sshDeleteRequest := cherrygo.DeleteSSHKey{ID: keyID}
+
+	c.SSHKey.Delete(&sshDeleteRequest)
+}
