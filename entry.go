8c12b1cc4e485761b7a8dd16e809e2269bb87945
Arturas Razinskij <arturas@kazkur.lt>
2019-03-03 14:02:17 +0200

    Show qty for plans in regions

--- entry.go
+++ entry.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 	"text/tabwriter"
 
 	"github.com/spf13/cobra"
@@ -421,31 +420,30 @@ func listPlans(c *cherrygo.Client, projectID int) {
 	}
 
 	tw := tabwriter.NewWriter(os.Stdout, 13, 8, 2, '\t', 0)
-	fmt.Fprintf(tw, "\n-------\t-------------\t----------\t-------------\t---\t-------\n")
-	fmt.Fprintf(tw, "Plan ID\tPlan name\tPlan price\tCPU\tRAM\tRegions\n")
-	fmt.Fprintf(tw, "-------\t-------------\t----------\t-------------\t---\t-------\n")
+	fmt.Fprintf(tw, "\n-------\t-------------\t----------\t-------------\t---\t-------\t---\n")
+	fmt.Fprintf(tw, "Plan ID\tPlan name\tPlan price\tCPU\tRAM\tRegions\tQty\n")
+	fmt.Fprintf(tw, "-------\t-------------\t----------\t-------------\t---\t-------\t---\n")
 	for _, p := range plans {
 
-		var regions []string
 		for _, r := range p.AvailableRegions {
-			regions = append(regions, r.Name)
-		}
-		reg := strings.Join(regions, ",")
 
-		var planPrice float32
-		for _, price := range p.Pricing {
-			if price.Unit == "Hourly" {
-				planPrice = price.Price
-				// Prints most expensive plans
-				if planPrice > 0 {
-					fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\t%v\n",
-						p.ID, p.Name, planPrice, p.Specs.Cpus.Name, p.Specs.Memory.Total, reg)
+			var planPrice float32
+			for _, price := range p.Pricing {
+				if price.Unit == "Hourly" {
+					planPrice = price.Price
+					// Prints most expensive plans
+					if planPrice > 0 {
+						fmt.Fprintf(tw, "%v\t%v\t%v\t%v\t%v\t%v\t%v\n",
+							p.ID, p.Name, planPrice, p.Specs.Cpus.Name, p.Specs.Memory.Total, r.Name, r.StockQty)
 
+					}
 				}
 			}
+
 		}
+
 	}
-	fmt.Fprintf(tw, "-------\t-------------\t----------\t-------------\t---\t-------\n")
+	fmt.Fprintf(tw, "-------\t-------------\t----------\t-------------\t---\t-------\t---\n")
 	tw.Flush()
 }
 
