a19633a99640cc8f1d1dedca016897d2a38334bc
Martynas Deveikis <martynasdev@gmail.com>
2024-07-22 11:14:25 +0300

    restructured init command

--- internal/init/init.go
+++ internal/init/init.go
@@ -50,90 +50,125 @@ func (c *Client) NewCommand() *cobra.Command {
   project-id: 123`,
 
 		DisableFlagsInUseLine: true,
-		
-	    RunE: func(cmd *cobra.Command, args []string) error {
-            homeDir, err := os.UserHomeDir()
-            if err != nil {
-                return fmt.Errorf("Error finding home directory: %s", err)
-            }
- 
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			homeDir, err := os.UserHomeDir()
+			if err != nil {
+				return fmt.Errorf("Error finding home directory: %s", err)
+			}
+
 			configDir := filepath.Join(homeDir, ".config", "cherry")
 			err = c.checkAndCreateConfig(configDir)
-            if err != nil {
-                return err
-            }
-			
-			config, _ := cmd.Flags().GetString("context")
-			if config != "" {
-				config = c.Servicer.ConfigFilePath(config, true)
-			}
-
-			fmt.Print("Cherry Servers API Tokens can be obtained through the portal at http://portal.cherryservers.com/.\n\n")
-			fmt.Print("Token (hidden): ")
-			b, err := term.ReadPassword(int(syscall.Stdin))
 			if err != nil {
 				return err
 			}
-			fmt.Println()
-			token := string(b)
-			c.Servicer.SetToken(token)
-			cherryclient := c.Servicer.API(cmd)
-			c.UserService = cherryclient.Users
-			_, _, err = c.UserService.CurrentUser(nil)
-			if err != nil {
-				return errors.Wrap(err, "Invalid authentication token")
+
+			context, _ := cmd.Flags().GetString("context")
+			if context != "" {
+				context = c.Servicer.ConfigFilePath(context, true)
 			}
 
-			teams, _, err := cherryclient.Teams.List(&cherrygo.GetOptions{})
-			if err != nil {
-				return errors.Wrap(err, "Failed to get team list")
-			}
-			fmt.Printf("Choose Team ID from list below:")
-			tw := tabwriter.NewWriter(os.Stdout, 8, 8, 0, '\t', 0)
-			fmt.Fprintf(tw, "\n-------\t---------\t---------------\n")
-			fmt.Fprintf(tw, "Team ID\tTeam name\tRemaining credit\n")
-			fmt.Fprintf(tw, "-------\t---------\t---------------\n")
-
-			for _, t := range teams {
-				fmt.Fprintf(tw, "%v\t%v\t%v\n",
-					t.ID, t.Name, (t.Credit.Account.Remaining + t.Credit.Promo.Remaining))
-			}
-			tw.Flush()
-			fmt.Printf("\n")
-			fmt.Printf("Team ID: ")
-			userTeam := ""
-			fmt.Scanln(&userTeam)
-
-			teamID, _ := strconv.Atoi(userTeam)
-			projects, _, err := cherryclient.Projects.List(teamID, &cherrygo.GetOptions{})
-			if err != nil {
-				return errors.Wrap(err, "Failed to get project list")
-			}
-			fmt.Printf("Choose Project ID from list below:")
-			tw = tabwriter.NewWriter(os.Stdout, 8, 8, 0, '\t', 0)
-			fmt.Fprintf(tw, "\n-------\t---------\n")
-			fmt.Fprintf(tw, "Project ID\tProject name\n")
-			fmt.Fprintf(tw, "-------\t---------\n")
-
-			for _, p := range projects {
-				fmt.Fprintf(tw, "%v\t%v\n",
-					p.ID, p.Name)
-			}
-			tw.Flush()
-			fmt.Printf("\n")
-			fmt.Printf("Project ID: ")
-			userProj := ""
-			fmt.Scanln(&userProj)
-
-			b, err = formatConfig(userProj, userTeam, token)
+			token, err := c.getAPIToken()
 			if err != nil {
 				return err
 			}
-			return writeConfig(config, b)
+			if err = c.validateToken(cmd); err != nil {
+				return err
+			}
+
+			cherryClient := c.Servicer.API(cmd)
+
+			userTeam, err := c.getUserTeam(cherryClient)
+			if err != nil {
+				return err
+			}
+
+			userProj, err := c.getUserProject(cherryClient, userTeam)
+			if err != nil {
+				return err
+			}
+
+			b, err := formatConfig(userProj, userTeam, token)
+			if err != nil {
+				return err
+			}
+			return writeConfig(context, b)
 		},
 	}
 
 	return initCmd
+}
+
+func (c *Client) getAPIToken() (string, error) {
+	fmt.Print("Cherry Servers API Tokens can be obtained through the portal at http://portal.cherryservers.com/.\n\n")
+	fmt.Print("Token (hidden): ")
+	b, err := term.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		return string(b), err
+	}
+	fmt.Println()
+	token := string(b)
+	c.Servicer.SetToken(token)
+
+	return token, nil
+}
+
+func (c *Client) validateToken(cmd *cobra.Command) error {
+	cherryClient := c.Servicer.API(cmd)
+	c.UserService = cherryClient.Users
+	_, _, err := c.UserService.CurrentUser(nil)
+	if err != nil {
+		return errors.Wrap(err, "Invalid authentication token")
+	}
+	return nil
+}
+
+func (c *Client) getUserTeam(cherryClient *cherrygo.Client) (string, error) {
+	teams, _, err := cherryClient.Teams.List(&cherrygo.GetOptions{})
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to get team list")
+	}
+	fmt.Printf("Choose Team ID from list below:")
+	tw := tabwriter.NewWriter(os.Stdout, 8, 8, 0, '\t', 0)
+	fmt.Fprintf(tw, "\n-------\t---------\t---------------\n")
+	fmt.Fprintf(tw, "Team ID\tTeam name\tRemaining credit\n")
+	fmt.Fprintf(tw, "-------\t---------\t---------------\n")
+
+	for _, t := range teams {
+		fmt.Fprintf(tw, "%v\t%v\t%v\n",
+			t.ID, t.Name, (t.Credit.Account.Remaining + t.Credit.Promo.Remaining))
+	}
+	tw.Flush()
+	fmt.Printf("\n")
+	fmt.Printf("Team ID: ")
+	userTeam := ""
+	fmt.Scanln(&userTeam)
+
+	return userTeam, nil
+}
+
+func (c *Client) getUserProject(cherryClient *cherrygo.Client, userTeam string) (string, error) {
+	teamID, _ := strconv.Atoi(userTeam)
+	projects, _, err := cherryClient.Projects.List(teamID, &cherrygo.GetOptions{})
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to get project list")
+	}
+	fmt.Printf("Choose Project ID from list below:")
+	tw := tabwriter.NewWriter(os.Stdout, 8, 8, 0, '\t', 0)
+	fmt.Fprintf(tw, "\n-------\t---------\n")
+	fmt.Fprintf(tw, "Project ID\tProject name\n")
+	fmt.Fprintf(tw, "-------\t---------\n")
+
+	for _, p := range projects {
+		fmt.Fprintf(tw, "%v\t%v\n",
+			p.ID, p.Name)
+	}
+	tw.Flush()
+	fmt.Printf("\n")
+	fmt.Printf("Project ID: ")
+	userProj := ""
+	fmt.Scanln(&userProj)
+	return userProj, nil
 }
 
 func formatConfig(userProj, userTeam, token string) ([]byte, error) {
@@ -159,27 +194,27 @@ func writeConfig(config string, b []byte) error {
 }
 
 func (c *Client) checkAndCreateConfig(configDir string) error {
-    if _, err := os.Stat(configDir); os.IsNotExist(err) {
-        if err := os.MkdirAll(configDir, 0o700); err != nil {
-            return fmt.Errorf("could not create directory %q: %s", configDir, err)
-        }
-    }
+	if _, err := os.Stat(configDir); os.IsNotExist(err) {
+		if err := os.MkdirAll(configDir, 0o700); err != nil {
+			return fmt.Errorf("could not create directory %q: %s", configDir, err)
+		}
+	}
 
-    files, err := filepath.Glob(filepath.Join(configDir, "*.yaml"))
-    if err != nil {
-        return fmt.Errorf("error checking for YAML files: %s", err)
-    }
+	files, err := filepath.Glob(filepath.Join(configDir, "*.yaml"))
+	if err != nil {
+		return fmt.Errorf("error checking for YAML files: %s", err)
+	}
 
-    if len(files) == 0 {
+	if len(files) == 0 {
 		defaultConfigPath := filepath.Join(configDir, "default.yaml")
-        file, err := os.Create(defaultConfigPath)
-        if err != nil {
-            return fmt.Errorf("failed to create default config file %q: %s", defaultConfigPath, err)
-        }
-        defer file.Close()
-    }
+		file, err := os.Create(defaultConfigPath)
+		if err != nil {
+			return fmt.Errorf("failed to create default config file %q: %s", defaultConfigPath, err)
+		}
+		defer file.Close()
+	}
 
-    return nil
+	return nil
 }
 
 type Servicer interface {
