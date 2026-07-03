8217848eaeb3eeb4a28f9353566689e5979b729c
Martynas Deveikis <martynasdev@gmail.com>
2026-07-03 14:34:32 +0300

    refactor(plan): rename regionID var to region

--- internal/plans/list.go
+++ internal/plans/list.go
@@ -10,7 +10,7 @@ import (
 )
 
 func (c *Command) list() *cobra.Command {
-	var regionID string
+	var region string
 	var teamID int
 	var types []string
 	planGetCmd := &cobra.Command{
@@ -32,8 +32,8 @@ func (c *Command) list() *cobra.Command {
 				options.Type = types
 			}
 
-			if regionID != "" {
-				options.QueryParams = map[string]string{"region": regionID}
+			if region != "" {
+				options.QueryParams = map[string]string{"region": region}
 			}
 
 			plans, _, err := c.Client().List(teamID, options)
@@ -54,7 +54,7 @@ func (c *Command) list() *cobra.Command {
 				}
 
 				for _, r := range p.AvailableRegions {
-					if regionID == "" || regionID == r.Slug || regionID == strconv.Itoa(r.ID) {
+					if region == "" || region == r.Slug || region == strconv.Itoa(r.ID) {
 						data = append(data, []string{p.Slug, r.Slug, strconv.Itoa(r.StockQty), priceHour, strconv.Itoa(r.SpotQty), priceSpot})
 					}
 				}
@@ -65,7 +65,7 @@ func (c *Command) list() *cobra.Command {
 		},
 	}
 
-	planGetCmd.Flags().StringVarP(&regionID, "region", "r", "", "The Slug or ID of region.")
+	planGetCmd.Flags().StringVarP(&region, "region", "r", "", "The Slug or ID of region.")
 	planGetCmd.Flags().StringSliceVarP(&types, "type", "", []string{}, "Comma separated list of available plan types.")
 	planGetCmd.Flags().IntVarP(&teamID, "team-id", "t", 0, "The team's ID. Return plans prices based on team billing details.")
 
